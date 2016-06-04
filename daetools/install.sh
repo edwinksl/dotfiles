b242cc6f0c9dce0257a7bfbda206b3feb25046ea
Edwin Khoo <edwinksl@gmail.com>
2016-06-04 03:32:38 -0400

    Add installation script for DAE Tools

---
+++ daetools/install.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+svn checkout svn://svn.code.sf.net/p/daetools/code/trunk daetools-code
+cd daetools-code
+virtualenv venv -p python3  # create virtualenv for Python 3
+source venv/bin/activate  # activate virtualenv
+pip install numpy scipy matplotlib
+chmod +x install_dependencies_linux.sh compile_libraries_linux.sh compile_linux.sh
+./install_dependencies_linux.sh
+./compile_libraries_linux.sh --with-python-version 3.5 boost ref_blas_lapack umfpack idas superlu superlu_mt bonmin nlopt
+./compile_linux.sh --with-python-version 3.5 dae superlu superlu_mt ipopt bonmin nlopt
