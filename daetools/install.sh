dcfdbc421fed66099d3a8290466f682e5f552eb4
Edwin Khoo <edwinksl@gmail.com>
2016-06-13 18:47:30 -0400

    Rename shared library

--- daetools/install.sh
+++ daetools/install.sh
@@ -7,3 +7,7 @@ chmod +x install_dependencies_linux.sh compile_libraries_linux.sh compile_linux.
 ./install_dependencies_linux.sh
 ./compile_libraries_linux.sh --with-python-version 3.5 boost ref_blas_lapack umfpack idas superlu superlu_mt bonmin nlopt  # compile everything except Trilinos and deal.II
 ./compile_linux.sh --with-python-version 3.5 dae superlu superlu_mt ipopt bonmin nlopt  # compile everything except Trilinos and deal.II
+cd daetools-package/daetools/solibs/Linux_x86_64
+rm libboost_python-daetools-py35.so
+mv libboost_python-daetools-py35.so.1.52.0 libboost_python3-daetools-py35.so.1.52.0
+ln -s libboost_python3-daetools-py35.so.1.52.0 libboost_python3-daetools-py35.so
