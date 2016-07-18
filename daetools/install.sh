55b2a45b836b6dd84e3e5fd838d7421a33a4af61
Edwin Khoo <edwinksl@gmail.com>
2016-07-18 18:42:20 -0400

    Add code for compiling everything except deal.II

--- daetools/install.sh
+++ daetools/install.sh
@@ -5,5 +5,7 @@ svn checkout svn://svn.code.sf.net/p/daetools/code/trunk daetools-code
 cd daetools-code
 chmod +x install_dependencies_linux.sh compile_libraries_linux.sh compile_linux.sh
 ./install_dependencies_linux.sh
-./compile_libraries_linux.sh --with-python-version 3.5 boost ref_blas_lapack umfpack idas superlu superlu_mt bonmin nlopt  # compile everything except Trilinos and deal.II
-./compile_linux.sh --with-python-version 3.5 dae superlu superlu_mt ipopt bonmin nlopt  # compile everything except Trilinos and deal.II
+# ./compile_libraries_linux.sh --with-python-version 3.5 boost ref_blas_lapack umfpack idas superlu superlu_mt bonmin nlopt  # compile everything except Trilinos and deal.II
+# ./compile_linux.sh --with-python-version 3.5 dae superlu superlu_mt ipopt bonmin nlopt  # compile everything except Trilinos and deal.II
+./compile_libraries_linux.sh --with-python-version 3.5 boost ref_blas_lapack umfpack idas superlu superlu_mt bonmin nlopt trilinos  # compile everything except deal.II
+./compile_linux.sh --with-python-version 3.5 dae superlu superlu_mt trilinos ipopt bonmin nlopt  # compile everything except deal.II
