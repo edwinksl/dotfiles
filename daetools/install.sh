d79fd7c52814496fbf843bfe3f04998a22d9d485
Edwin Khoo <edwinksl@gmail.com>
2016-11-02 20:29:51 -0400

    Install dependencies

--- daetools/install.sh
+++ daetools/install.sh
@@ -6,6 +6,7 @@ cd daetools-code
 # chmod +x install_dependencies_linux.sh compile_libraries_linux.sh compile_linux.sh
 chmod +x compile_libraries_linux.sh compile_linux.sh
 # ./install_dependencies_linux.sh
+sudo apt-get install libqt4-dev qtcreator qt4-designer autotools-dev automake make pkg-config autoconf gcc g++ gfortran binutils cmake patch wget subversion fakeroot libfreetype6-dev swig python-dev python3-dev libpng-dev libxext-dev libbz2-dev
 # ./compile_libraries_linux.sh --with-python-version 3.5 boost ref_blas_lapack umfpack idas superlu superlu_mt bonmin nlopt  # compile everything except Trilinos and deal.II
 # ./compile_linux.sh --with-python-version 3.5 dae superlu superlu_mt ipopt bonmin nlopt  # compile everything except Trilinos and deal.II
 # ./compile_libraries_linux.sh --with-python-version 3.5 boost ref_blas_lapack umfpack idas superlu superlu_mt bonmin nlopt trilinos  # compile everything except deal.II
